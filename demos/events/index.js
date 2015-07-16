5e07a0da21333cf454e70ef5a2ea680b0a558593
sokis <zengyi.zal@gmail.com>
2015-07-16 17:22:52 +0800

    add  demos

---
+++ demos/events/index.js
@@ -0,0 +1,18 @@
+/**
+ * SunEee Js
+ * @data Create. 2015/7/16
+ * @author Ryan(鸿飞)<zengyi.zal@gmail.com>
+ **/
+
+var SurverEditor = React.createClass({
+
+    render() {
+        return (
+            <div></div>
+        );
+    }
+});
+React.render(
+    <SurverEditor />,
+    document.getElementById('example')
+);
